534b697654e4a21a55a3c7c143bae8fa21b797ed
Ishan Kumar <kumarishaan01@gmail.com>
2021-04-09 20:32:20 +0530

    second commit

--- app/src/main/java/com/example/splitter/Fragment/ParticulatTrip.kt
+++ app/src/main/java/com/example/splitter/Fragment/ParticulatTrip.kt
@@ -1,10 +1,13 @@
 package com.example.splitter.Fragment
 
 import android.R
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
@@ -79,8 +82,6 @@ class ParticulatTrip : Fragment() {
 
         return view
     }
-
-
 
 
 
